6d2e5c084674410418edcaa92c7dd7271d71d7c1
PIERPAOLO COLELLA <pc2034731@adt098.adt.unipd.it>
2022-11-16 13:04:04 +0100

    Book.h

--- Book.h
+++ Book.h
@@ -2,7 +2,39 @@
 #define Book_h
 #include <iostream>
 class Book{
-    
+public:
+//constructors
+    Book();
+    Book(std::string name = "", std::string surname = "",std::string book_title = "",ISBN code , Date copyright, bool available = true){};
+    //copy constructor
+    Book(const Book& b);
+    //move constructor
+    Book(Book&& b);
+//operators
+    //copy assignment
+    Book& operator=(const Book& b);
+    //move assignment
+    Book& operator=(Book&& b);
+//member functions
+    ISBN get_isbn() const;
+    Date get_date() const;
+    bool isavailable() const;
+    std::string get_name() const;
+    std::string get_surname() const;
+    std::string get_title() const;
+    //mette a false available, se lo e' gia' lancia un'eccezione
+    void borrow();
+    //mette a true available, se lo e' gia' lancia un'eccezione
+    void give_back();
+private:
+    std::string auth_name;
+    std::string auth_surname ;
+    std::string title;
+    ISBN isbn_code;
+    Date copyright_date;
+    bool available;
 };
-#include "Book.hpp"
+bool operator==(const Book& a,const Book& b){return b.get_isbn() == a.get_isbn(); }
+bool operator!=(const Book& a,const Book& b){return b.get_isbn() != a.get_isbn();}
+std::ostream& operator<<(std::ostream& os, const Book& b );
 #endif
