8463f09d221fef14850a8f1391c221da51d6f8f7
GLAScorpion <pier.colella122@gmail.com>
2022-11-21 11:53:06 +0100

    finito
    stavolta sì

--- book.h
+++ book.h
@@ -6,31 +6,47 @@
 class Book{
 public:
 //constructors
+
+    //costruttore di default: imposta stringe vuote e invoca costruttori di default di isbn e date
     Book();
+
+    //crea un Book con tre stringhe nome e cognome autore, titolo, un oggetto isbn. è possibile specificare la data di copyright e se è available
     Book(std::string name, std::string surname,std::string book_title, ISBN code , Date copyright = Date(), bool disponibile = true);
+
+    //come il costruttore precedente, ma con l'isbn in formato stringa
     Book(std::string name, std::string surname,std::string book_title, const std::string& code , Date copyright = Date(), bool disponibile = true);
+
 //member functions
-    ISBN get_isbn() const {return isbn_code;}
-    Date get_date() const {return copyright_date;}
-    bool is_available() const {return available;}
-    std::string get_name() const {return auth_name;}
-    std::string get_surname() const {return auth_surname;}
-    std::string get_title() const {return title;}
-    //mette a false available, se lo e' gia' lancia un'eccezione
+
+    ISBN get_isbn() const {return isbn_code_;}
+    Date get_date() const {return copyright_date_;}
+    bool is_available() const {return available_;}
+    std::string get_name() const {return auth_name_;}
+    std::string get_surname() const {return auth_surname_;}
+    std::string get_title() const {return title_;}
+
+    //mette a false available_, se lo e' gia' lancia un'eccezione
     void Borrow(); //prestito del libro
-    //mette a true available, se lo e' gia' lancia un'eccezione
+
+    //mette a true available_, se lo e' gia' lancia un'eccezione
     void GiveBack(); //restituzione del libro
+
 //operators
+
     bool operator==(const Book& b) const;
     bool operator!=(const Book& b) const;
+
 private:
-    std::string auth_name ;
-    std::string auth_surname ;
-    std::string title;
-    ISBN isbn_code;
-    Date copyright_date;
-    bool available;
+    std::string auth_name_ ;
+    std::string auth_surname_ ;
+    std::string title_;
+    ISBN isbn_code_;
+    Date copyright_date_;
+    bool available_;
 };
+
 //HELPER FUNCTIONS
+
 std::ostream& operator<<(std::ostream& os, const Book& b );
+
 #endif
--- date_tester.cpp
+++ date_tester.cpp
@@ -1,16 +1,58 @@
 #include <iostream>
 #include "date.h"
-using namespace std;
+
 int main(){
-    Date x = Date(12,10,1950);
-    cout<<x.get_month()<<endl;
-    Date b;
-    cout << &x << " " <<&b<<endl;
-    cout << x << " " <<b<<endl;
-    Date a{29,2,2000};
-    cout<<a<<endl;
-    a.set_month(5);
-    cout<<a<<endl;
-    a.set_month(Month::jan);
-    cout<<a<<endl;
+	//test della classe date
+	std::cout<<"Test della classe Date:"<<std::endl;
+	
+	//test del costruttore di default
+	std::cout<<std::endl<<"Test del costruttore di default:"<<std::endl;
+	Date date1 {};
+	std::cout<<date1<<std::endl;
+	
+	//test del costruttore
+	std::cout<<std::endl<<"Test del costruttore:"<<std::endl;
+	Date date2 {1,2,1890};
+	std::cout<<date2<<std::endl;
+	
+	//test delle member function get_month, get_day, get_year
+	std::cout<<std::endl<<"Test delle member function get_month, get_day, get_year(con la data 25-9-2001):"<<std::endl;
+	Date date3 {25,9,2001};
+	std::cout<<"Il giorno: "<<date3.get_day()<<", il mese: "<<date3.get_month()<<", l'anno: "<<date3.get_year()<<std::endl;
+	
+	//test della member function IsValid
+	std::cout<<std::endl<<"Test della member function IsValid(con la data 34-1-1970)"<<std::endl;
+
+	try{
+		Date date4{34,1,1970};
+	}
+    catch(const std::invalid_argument& i){
+		std::cout<<"La creazione della data lancia un eccezione: "<<i.what()<<std::endl;
+	}
+
+	std::cout<<"Il controllo tramite IsValid resituisce che: ";
+	if(!IsValid(34,1,1970)){
+		std::cout<<"La data non e' valida"<<std::endl;
+	}else{
+		std::cout<<"La data e' valida"<<std::endl;
+	}
+
+	//test della member function IsBisestile
+	std::cout<<std::endl<<"Test della member function IsBisestile (con la data 1-2-2020):"<<std::endl;
+	Date date5 {1,2,2020};
+	if(IsBisestile(date5.get_year())){
+		std::cout<<"L'anno e' bisestile"<<std::endl;
+	}else{
+		std::cout<<"L'anno non e' bisestile"<<std::endl;
+	}
+
+    std::cout<<std::endl<<"Test della member function IsBisestile (con la data 1-2-2019):"<<std::endl;
+	Date date6 {1,2,2019};
+	if(IsBisestile(date6.get_year())){
+		std::cout<<"L'anno e' bisestile"<<std::endl;
+	}else{
+		std::cout<<"L'anno non e' bisestile"<<std::endl;
+	}
+
+    return 0;
 }
